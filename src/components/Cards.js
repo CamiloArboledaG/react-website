a210407734517472d16663a2da12e668c28f04bd
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2022-04-30 19:19:15 -0500

    footer, neon effect and a card3d

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
+import Cards3d from './Cards3d';
 
 function Cards() {
     return (
@@ -10,15 +11,15 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem src="images/img-9.jpg" text="Explore the hidden jungle wondeful" label="Adventure" path='/services' />
-                    </ul>
-                    <ul className="cards__items">
-                        <CardItem src="images/img-9.jpg" text="Explore the hidden jungle wondeful" label="Adventure" path='/services' />
                         <CardItem src="images/img-9.jpg" text="Explore the hidden jungle wondeful" label="Adventure" path='/services' />
                     </ul>
                     <ul className="cards__items">
                         <CardItem src="images/img-9.jpg" text="Explore the hidden jungle wondeful" label="Adventure" path='/services' />
                         <CardItem src="images/img-9.jpg" text="Explore the hidden jungle wondeful" label="Adventure" path='/services' />
                         <CardItem src="images/img-9.jpg" text="Explore the hidden jungle wondeful" label="Adventure" path='/services' />
+                    </ul>
+                    <ul>
+                        <Cards3d></Cards3d>
                     </ul>
                 </div>
             </div>
---
+++ src/components/Cards3d.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Cards3d.css'
+
+function Cards3d() {
+    return (
+        <section className="cards3d">
+            <figure className="card3d">
+                <figcaption className="card3d-title">Check out this card 3d</figcaption>
+            </figure>
+        </section>
+    )
+}
+
+export default Cards3d
